7b96e7f01a129fd51007a6ddfceab9796075fcc3
Ernesto Neto <ernestoborges1995@gmail.com>
2024-09-28 14:41:34 -0300

    feat: Add log filter to log list

---
+++ src/components/atoms/ToggleButton.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function ToggleButton({
+    children,
+    value,
+    className,
+    onClick,
+}: {
+    children: React.ReactNode
+    value: boolean
+    className?: string,
+    onClick: () => void
+}) {
+    const color1 = '#23823a';
+    const color2 = '#75e9a0';
+    return (
+        <>
+            <div
+                className={`font-bold py-1 px-2 cursor-pointer text-base rounded-lg ${className}`}
+                style={{
+                    backgroundColor: value ? color2 : "#80808050",
+                    color: value ? color1 : "#a4a4a4"
+                }}
+                onClick={onClick}
+            >
+                {children}
+            </div>
+        </>
+    )
+}
--- src/components/organism/LogChat.tsx
+++ src/components/organism/LogChat.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { IoMdSend } from "react-icons/io";
 import useSocket from "../../hooks/useSocket"
 import MessageDisplay from "../molecules/MessageDisplay";
+import ToggleButton from "../atoms/ToggleButton";
 
 interface Message {
     sender: string,
@@ -14,6 +15,13 @@ export default function LogChat() {
 
     const { socket, token, connected } = useSocket();
     const [log, setLog] = useState<any[]>([]);
+    const [filter, setFilter] = useState({
+        all: true,
+        action: false,
+        player: false,
+        error: false,
+        system: false,
+    })
 
     useEffect(() => {
         if (socket) {
@@ -25,10 +33,55 @@ export default function LogChat() {
 
     return (
         <div className="border rounded-lg p-4 flex flex-col gap-4 items-center w-[30rem] h-[20rem]">
+            <div className="w-full">
+                <ul className="w-full flex gap-4">
+                    <li>
+                        <ToggleButton
+                            onClick={() => setFilter(prev => ({ ...prev, all: !prev.all }))}
+                            value={filter.all}
+                        >
+                            tudo
+                        </ToggleButton>
+                    </li>
+                    <li>
+                        <ToggleButton
+                            onClick={() => setFilter(prev => ({ ...prev, action: !prev.action }))}
+                            value={filter.action}
+                        >
+                            ação
+                        </ToggleButton>
+                    </li>
+                    <li>
+                        <ToggleButton
+                            onClick={() => setFilter(prev => ({ ...prev, player: !prev.player }))}
+                            value={filter.player}
+                        >
+                            jogador
+                        </ToggleButton>
+                    </li>
+                    <li>
+                        <ToggleButton
+                            onClick={() => setFilter(prev => ({ ...prev, system: !prev.system }))}
+                            value={filter.system}
+                        >
+                            sistema
+                        </ToggleButton>
+                    </li>
+                </ul>
+            </div>
             <MessageDisplay>
-
                 {
-                    log.map((c, i) =>
+                    log.filter(c => {
+                        if (filter.all) {
+                            return true
+                        } else {
+                            switch (c.type) {
+                                case 'action': return filter.action
+                                case 'player': return filter.player
+                                case 'system': return filter.system
+                            }
+                        }
+                    }).map((c, i) =>
                         <li key={i}>
                             <div className="break-words">
                                 <span className="font-bold">{c.message}</span>
@@ -37,6 +90,6 @@ export default function LogChat() {
                     )
                 }
             </MessageDisplay>
-        </div>
+        </div >
     )
 }
